afb24bbc56974e256bcce7e4edb0e2135e6c1dde
karthi5555 <105117281+karthi5555@users.noreply.github.com>
2023-12-11 09:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+var num1 = prompt("enter first value");
+var res = parseInt(num1) * 2;
+var num2 = prompt("enter second value");
+res = res + parseInt(num2);
+alert("The final result is: " + res);
